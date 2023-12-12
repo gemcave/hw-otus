62041e7978375bc3c72caef1a3ec2df061eee54d
Aldiyar Y <a.erzhanov@it-alnc.ru>
2023-12-12 14:34:11 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,38 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var sb strings.Builder
+	for i := 0; i < len(s); i++ {
+		num, err := strconv.Atoi(string(s[i]))
+
+		if err != nil && i+1 < len(s) {
+			if _, err := strconv.Atoi(string(s[i+1])); err != nil {
+				sb.WriteString(string(s[i]))
+				continue
+			}
+		}
+		
+		if err == nil {
+			if i == 0 {
+				return "", ErrInvalidString
+			}
+			if _, err := strconv.Atoi(string(s[i-1])); err == nil {
+				return "", ErrInvalidString
+			}
+		}
+
+		if i+1 == len(s) {
+			sb.WriteString(string(s[i]))
+		} else if i != 0 {
+			sb.WriteString(strings.Repeat(string(s[i-1]), num))
+		}
+	}
+	return sb.String(), nil
 }
