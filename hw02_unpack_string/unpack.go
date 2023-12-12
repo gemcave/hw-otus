19c6a870c051a6c9697980fa69d53ba2018c94a8
Aldiyar Y <a.erzhanov@it-alnc.ru>
2023-12-12 23:14:58 +0300

    HW2: добавил тесты + доработал алгоритм

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,12 +10,14 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
 	var sb strings.Builder
-	for i := 0; i < len(s); i++ {
-		num, err := strconv.Atoi(string(s[i]))
+	sliceRune := []rune(s)
 
-		if err != nil && i+1 < len(s) {
-			if _, err := strconv.Atoi(string(s[i+1])); err != nil {
-				sb.WriteString(string(s[i]))
+	for i := 0; i < len(sliceRune); i++ {
+		num, err := strconv.Atoi(string(sliceRune[i]))
+
+		if err != nil && i+1 < len(sliceRune) {
+			if _, err := strconv.Atoi(string(sliceRune[i+1])); err != nil {
+				sb.WriteRune(sliceRune[i])
 				continue
 			}
 		}
@@ -24,15 +26,17 @@ func Unpack(s string) (string, error) {
 			if i == 0 {
 				return "", ErrInvalidString
 			}
-			if _, err := strconv.Atoi(string(s[i-1])); err == nil {
+			if _, err := strconv.Atoi(string(sliceRune[i-1])); err == nil {
 				return "", ErrInvalidString
 			}
 		}
 
-		if i+1 == len(s) {
-			sb.WriteString(string(s[i]))
+		if i+1 == len(sliceRune) && err != nil {
+			if err != nil {
+				sb.WriteRune(sliceRune[i])
+			}
 		} else if i != 0 {
-			sb.WriteString(strings.Repeat(string(s[i-1]), num))
+			sb.WriteString(strings.Repeat(string(sliceRune[i-1]), num))
 		}
 	}
 	return sb.String(), nil
